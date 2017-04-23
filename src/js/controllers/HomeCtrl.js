fcd2dd4f6d40958ac306f0fd889e58d4a02afe0a
TaoJ <jingt@upmc.edu>
2017-04-23 18:59:05 -0400

    get target l from result

--- src/js/controllers/HomeCtrl.js
+++ src/js/controllers/HomeCtrl.js
@@ -5,6 +5,11 @@ angular.module('home',['common'])
       $scope.message = 'hi';
       $scope.res='good lucks';
 
+      $scope.languages = {};
+
+      $scope.languages['zh-CN'] = '';
+      $scope.languages['ja'] = '';
+
       $scope.submit = function () {
           var url = endpoints.getTranslateUrl('auto','zh-CN', $scope.source);
 
@@ -13,8 +18,23 @@ angular.module('home',['common'])
                   $scope.message = error;
               })
               .then(function(res){
-                    $scope.res = res.data[0][0][0];
+               //     $scope.res = res.data[0][0][0];
+	              var targetL = res.config.url.split("=")[3];
+	              targetL = targetL.substring(0, targetL.indexOf("&"));
+                  $scope.res = targetL;
               });
       }
+
+      var process = function(tl){
+	      var url = endpoints.getTranslateUrl('en',tl, $scope.source);
+
+	      $http.get(url)
+		      .catch(function (error) {
+			      $scope.message = error;
+		      })
+		      .then(function(res){
+			      $scope.res = res.data[0][0][0];
+		      });
+      };
     }
   ])
