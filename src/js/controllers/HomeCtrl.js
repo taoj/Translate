241b9b8af54628b095a02455e2ef9961d181a6fc
tao <irrefuse@gmail.com>
2017-04-26 08:05:53 -0400

    4-26

--- src/js/controllers/HomeCtrl.js
+++ src/js/controllers/HomeCtrl.js
@@ -1,4 +1,4 @@
-angular.module('home',['common'])
+angular.module('home',['multipleSelect','common'])
   .controller('HomeCtrl',[
     '$scope', 'endpoints', '$http',
     function($scope, endpoints, $http){
@@ -6,23 +6,39 @@ angular.module('home',['common'])
       $scope.res='good lucks';
 
       $scope.languages = {};
+      $scope.selected=[];
+      $scope.langOpetions= [
+        {name:'Arabic', code:'ar'},
+        {name:'Chinese (Simplified)',code:'zh-CN'},
+        {name:'Dutch',code:'nl'},
+        {name:'French',code:'fr'},
+        {name:'German',code:'de'},
+        {name:'Italian',code:'it'},
+        {name:'Japanese',code:'ja'},
+        {name:'Korean',code:'ko'}
+        ];
 
       $scope.languages['zh-CN'] = '';
       $scope.languages['ja'] = '';
 
       $scope.submit = function () {
-          var url = endpoints.getTranslateUrl('auto','zh-CN', $scope.source);
 
-          $http.get(url)
-              .catch(function (error) {
-                  $scope.message = error;
-              })
-              .then(function(res){
-               //     $scope.res = res.data[0][0][0];
-	              var targetL = res.config.url.split("=")[3];
-	              targetL = targetL.substring(0, targetL.indexOf("&"));
-                  $scope.res = targetL;
-              });
+          for(var key in $scope.languages){
+            if($scope.languages.hasOwnProperty(key)){
+              var url = endpoints.getTranslateUrl('en',key, $scope.source);
+              $http.get(url)
+                  .catch(function (error) {
+                      $scope.message = error;
+                  })
+                  .then(function(res){
+    	              var targetL = res.config.url.split("=")[3];
+    	              targetL = targetL.substring(0, targetL.indexOf("&"));
+                      $scope.res = targetL;
+                      $scope.languages[targetL] = res.data[0][0][0];
+                  });
+            }
+          }
+
       }
 
       var process = function(tl){
